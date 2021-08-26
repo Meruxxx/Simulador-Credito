4c3ff4701ad6a244589513de6f54bcdbf2d8f8c8
Meruxxx <diegoma.04@gmail.com>
2021-08-26 11:44:23 -0500

    se agregan los tipos de credito restantes y se configuran la tasa de interes de cada uno

--- src/app/core/types/credito.types.ts
+++ src/app/core/types/credito.types.ts
@@ -1,3 +1,8 @@
-export type TipoCredito = 'LIBRE_INVERSION' | 'COMERCIO' | 'VIVIENDA';
+export type TipoCredito =
+  | 'LIBRE_INVERSION'
+  | 'VIVIENDA'
+  | 'COMERCIO'
+  | 'EDUCATIVO'
+  | 'CREDIFACIL';
 
 export type TipoDeuda = 'HIPOTECA' | 'DEUDOR_SOLIDARIO' | 'NINGUNA';
