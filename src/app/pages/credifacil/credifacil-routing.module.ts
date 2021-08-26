ac9a7f7c9d147bb40829ccd02076e05390923042
Alexis Ceballos <e.alexisceballos@egmail.com>
2021-08-26 14:11:25 -0500

    agregar componetes

--- src/app/pages/credifacil/credifacil-routing.module.ts
+++ src/app/pages/credifacil/credifacil-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CredifacilPage } from './credifacil.page';
 
 const routes: Routes = [{ path: '', component: CredifacilPage }];
-
+// ¨¨¨¨
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
