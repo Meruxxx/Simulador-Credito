e409540584af66f34899c8e0a9ae681447044add
Alexis Ceballos <e.alexisceballos@egmail.com>
2021-09-01 09:22:54 -0500

    Checkbox

--- src/app/components/enviar-correo/enviar-correo.page.ts
+++ src/app/components/enviar-correo/enviar-correo.page.ts
@@ -1,28 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EmailService } from 'src/app/core/services/email.service';
 
 @Component({
   selector: 'app-enviar-correo',
   templateUrl: './enviar-correo.page.html',
-  styleUrls: ['./enviar-correo.page.css']
+  styleUrls: ['./enviar-correo.page.css'],
 })
-export class EnviarCorreoPage implements OnInit {
-  @Input() visible = false
-  @Input() title : any;
+export class EnviarCorreoPage {
+  @Input() visible = false;
+  @Input() title: any;
+  buttonVisible = true;
+  checked = false;
   form: FormGroup;
   constructor(
     private emailService: EmailService,
-    private formBuilder: FormBuilder,) {
-      this.form = this.formBuilder.group({
-        NombreCliente: ['', [Validators.required]],
-        emailCliente: ['', [Validators.required, Validators.email]],
-        TelCel: ['', [Validators.required, Validators.maxLength(256)]],
-      });
-     }
-
-  ngOnInit(): void {
+    private formBuilder: FormBuilder
+  ) {
+    this.form = this.formBuilder.group({
+      NombreCliente: ['', [Validators.required]],
+      emailCliente: ['', [Validators.required, Validators.email]],
+      TelCel: ['', [Validators.required, Validators.maxLength(256)]],
+    });
   }
+
   onClickContacto(): void {
     const { NombreCliente, emailCliente, TelCel } = this.form.value;
     this.emailService
@@ -36,9 +37,9 @@ export class EnviarCorreoPage implements OnInit {
       })
       .then(console.log);
   }
-  checked = false;
 
   toggle(checked: boolean) {
+    this.buttonVisible = false;
     this.checked = checked;
   }
 }
