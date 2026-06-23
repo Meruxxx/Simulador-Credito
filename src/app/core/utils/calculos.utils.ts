b2f5f1bef4d3a408ea72ff2c4422215619fb098f
ShapesSG <108829646+ShapesSG@users.noreply.github.com>
2026-06-23 13:46:17 -0500

    Limpieza de código con Gemini y tasas actualizadas

--- src/app/core/utils/calculos.utils.ts
+++ src/app/core/utils/calculos.utils.ts
@@ -1,6 +1,6 @@
 import { TipoCredito, TipoDeuda } from '../types/credito.types';
 import { TipoAhorro } from './../types/credito.types';
- 
+
 export const tasaInteresLibreInversion: Record<
   number,
   { tasaMensual: number; tasaEA: number }
@@ -11,12 +11,14 @@ export const tasaInteresLibreInversion: Record<
   60: { tasaMensual: 1.78, tasaEA: 23.54 },
   72: { tasaMensual: 2.13, tasaEA: 28.84 }, // Pendiente 23 jun 2026
 };
+
 export const tasaInteresVivienda: Record<
   number,
   { tasaMensual: number; tasaEA: number }
 > = {
   84: { tasaMensual: 1.15, tasaEA: 14.70 },
 };
+
 export const tasaInteresComercio: Record< // Pendiente 23 jun 2026. Cambiar nombre Crédito Popular Productivo
   number,
   { tasaMensual: number; tasaEA: number }
@@ -27,6 +29,7 @@ export const tasaInteresComercio: Record< // Pendiente 23 jun 2026. Cambiar nomb
   60: { tasaMensual: 2.29, tasaEA: 31.15 }, // quitar
   72: { tasaMensual: 2.08, tasaEA: 28.08 }, // quitar
 };
+
 export const tasaInteresEducativo: Record< // Hay más meses. Falta adicionar para cálculo
   number,
   { tasaMensual: number; tasaEA: number }
@@ -35,6 +38,7 @@ export const tasaInteresEducativo: Record< // Hay más meses. Falta adicionar pa
   24: { tasaMensual: 1.19, tasaEA: 15.26 },
   36: { tasaMensual: 1.29, tasaEA: 16.69 },
 };
+
 export const tasaInteresCrediFacil: Record<
   number,
   { tasaMensual: number; tasaEA: number }
@@ -48,16 +52,18 @@ export const parametrosLibreInversion: Record<
   TipoDeuda,
   { plazoMaximo: number; montoMaximo: number }
 > = {
-  HIPOTECA: { plazoMaximo: 60, montoMaximo: 408836700 }, // Se pasaron todos de 72 a 60 el 23jun2026
+  HIPOTECA: { plazoMaximo: 60, montoMaximo: 408836700 }, // Se pasaron todos de 72 a 36 el 23jun2026
   DEUDOR_SOLIDARIO: { plazoMaximo: 60, montoMaximo: 408836700 },
   NINGUNA: { plazoMaximo: 60, montoMaximo: 408836700 },
 };
+
 export const parametrosVivienda: Record<
   'NINGUNA',
   { plazoMaximo: number; montoMaximo: number }
 > = {
   NINGUNA: { plazoMaximo: 84, montoMaximo: 122651010 },
 };
+
 export const parametrosComercio: Record<
   TipoDeuda,
   { plazoMaximo: number; montoMaximo: number }
@@ -66,12 +72,14 @@ export const parametrosComercio: Record<
   DEUDOR_SOLIDARIO: { plazoMaximo: 36, montoMaximo: 408836700 },
   NINGUNA: { plazoMaximo: 36, montoMaximo: 408836700 },
 };
+
 export const parametrosEducativo: Record<
   'NINGUNA',
   { plazoMaximo: number; montoMaximo: number }
 > = {
   NINGUNA: { plazoMaximo: 36, montoMaximo: 999999999 }, // se pasó de 12 a 36 el 23jun2026
 };
+
 export const parametrosCrediFacil: Record<
   'NINGUNA',
   { plazoMaximo: number; montoMaximo: number }
@@ -82,9 +90,11 @@ export const parametrosCrediFacil: Record<
 export const parametrosAhorro: Record<'NINGUNA', { montoMinimo: number }> = {
   NINGUNA: { montoMinimo: 272557 },
 };
+
 export const parametrosAhorroContractuales: Record<'NINGUNA', { montoMinimo: number }> = {
   NINGUNA: { montoMinimo: 10000 },
 };
+
 export const tasaInteresAhorro: Record<number, { tasaEA: number }> = {
   30: { tasaEA: 1.43 },
   60: { tasaEA: 1.75 },
@@ -97,6 +107,7 @@ export const tasaInteresAhorro: Record<number, { tasaEA: number }> = {
   540: { tasaEA: 8.91 },
   720: { tasaEA: 8.91 },
 };
+
 export const tasaInteresAhorroContractuales: Record<number, { tasaEA: number }> = {
   6: { tasaEA: 4.00 },
   9: { tasaEA: 4.50 },
@@ -104,96 +115,70 @@ export const tasaInteresAhorroContractuales: Record<number, { tasaEA: number }> 
   15: { tasaEA: 5.50 },
   18: { tasaEA: 5.50 },
   24: { tasaEA: 5.50 },
-  36: { tasaEA: 6.50 }
+  36: { tasaEA: 6.50 },
 };
 
 export const CALCULOS_UTILS = {
   //TODO: Se debe retornar tambien la informacion del error ocurrido y mostrado en un Toast
   getintervalo(tipoCredito: TipoCredito, numerocuota: number): number {
-    let intervalocuota:number=0;
+    let intervalocuota: number = 0;
     switch (tipoCredito) {
       case 'LIBRE_INVERSION':
         if (numerocuota < 13) {
           intervalocuota = 12;
-        }
-        else if (numerocuota < 25) {
+        } else if (numerocuota < 25) {
           intervalocuota = 24;
-        }
-        else if (numerocuota <37){
+        } else if (numerocuota < 37) {
           intervalocuota = 36;
+        } else if (numerocuota < 61) {
+          intervalocuota = 60;
+        } else if (numerocuota < 73) {
+          intervalocuota = 72;
+        } else if (numerocuota > 72) {
+          intervalocuota = 72;
         }
-
-          else if (numerocuota <61){
-            intervalocuota = 60;
-          }
-
-          else if (numerocuota <73){
-            intervalocuota = 72;
-          }
-
-          else if (numerocuota >72){
-            intervalocuota = 72;
-          }
         break;
-        case 'VIVIENDA':
-          intervalocuota = 84;
-          break;
+      case 'VIVIENDA':
+        intervalocuota = 84;
+        break;
       case 'COMERCIO':
         if (numerocuota < 13) {
           intervalocuota = 12;
-        }
-        else if (numerocuota < 25) {
+        } else if (numerocuota < 25) {
           intervalocuota = 24;
-        }
-        else if (numerocuota <37){
+        } else if (numerocuota < 37) {
           intervalocuota = 36;
+        } else if (numerocuota < 61) {
+          intervalocuota = 60;
+        } else if (numerocuota < 73) {
+          intervalocuota = 72;
+        } else if (numerocuota > 72) {
+          intervalocuota = 72;
         }
-
-          else if (numerocuota <61){
-            intervalocuota = 60;
-          }
-
-          else if (numerocuota <73){
-            intervalocuota = 72;
-          }
-
-          else if (numerocuota >72){
-            intervalocuota = 72;
-          }
         break;
       case 'EDUCATIVO':
         if (numerocuota < 13) {
           intervalocuota = 12;
-        }
-        else if (numerocuota < 25) {
+        } else if (numerocuota < 25) {
           intervalocuota = 24;
-        }
-        else if (numerocuota <37){
+        } else if (numerocuota < 37) {
           intervalocuota = 36;
         }
         break;
       case 'CREDIFACIL':
         if (numerocuota < 13) {
           intervalocuota = 12;
-        }
-        else if (numerocuota < 25) {
+        } else if (numerocuota < 25) {
           intervalocuota = 24;
-        }
-        else if (numerocuota <37){
+        } else if (numerocuota < 37) {
           intervalocuota = 36;
+        } else if (numerocuota < 61) {
+          intervalocuota = 60;
+        } else if (numerocuota < 73) {
+          intervalocuota = 72;
+        } else if (numerocuota > 72) {
+          intervalocuota = 73;
         }
-
-          else if (numerocuota <61){
-            intervalocuota = 60;
-          }
-
-          else if (numerocuota <73){
-            intervalocuota = 72;
-          }
-
-          else if (numerocuota >72){
-            intervalocuota = 73;
-          }
         break;
     }
     return intervalocuota;
@@ -204,7 +189,7 @@ export const CALCULOS_UTILS = {
     tipoDeuda: TipoDeuda,
     monto: number,
     numeroCuotas: number
-  ): [valor:number,interes:number,interesesEA:number,error:string]{
+  ): [valor: number, interes: number, interesesEA: number, error: string] {
     let parametros;
     let tasaMensual;
     let tasaEA;
@@ -214,14 +199,11 @@ export const CALCULOS_UTILS = {
       case 'LIBRE_INVERSION':
         parametros = parametrosLibreInversion[tipoDeuda];
 
-        if (
-          parametros.plazoMaximo < numeroCuotas
-
-        ) {
-          return  [0,0,0,'El número de cuotas supera el plazo maximo establecido para el crédito.'];
+        if (parametros.plazoMaximo < numeroCuotas) {
+          return [0, 0, 0, 'El número de cuotas supera el plazo maximo establecido para el crédito.'];
         }
         if (parametros.montoMaximo < monto) {
-          return [0,0,0,'El monto insgresado supera el monto maximo establecido para el crédito.'];
+          return [0, 0, 0, 'El monto insgresado supera el monto maximo establecido para el crédito.'];
         }
 
         tasaMensual = tasaInteresLibreInversion[intervalocuota].tasaMensual;
@@ -229,31 +211,23 @@ export const CALCULOS_UTILS = {
         break;
       case 'VIVIENDA':
         parametros = parametrosVivienda['NINGUNA'];
-        // console.log(parametros);
-        if (
-          parametros.plazoMaximo < numeroCuotas
-
-        ) {
-          return  [0,0,0,'El número de cuotas supera el plazo maximo establecido para el crédito.'];
+        if (parametros.plazoMaximo < numeroCuotas) {
+          return [0, 0, 0, 'El número de cuotas supera el plazo maximo establecido para el crédito.'];
         }
         if (parametros.montoMaximo < monto) {
-          return [0,0,0,'El monto insgresado supera el monto maximo establecido para el crédito.'];
+          return [0, 0, 0, 'El monto insgresado supera el monto maximo establecido para el crédito.'];
         }
         tasaMensual = tasaInteresVivienda[intervalocuota].tasaMensual;
         tasaEA = tasaInteresVivienda[intervalocuota].tasaEA;
-        // console.log('la variables de tasa son' + tasaMensual + tasaEA);
         break;
       case 'COMERCIO':
         parametros = parametrosComercio[tipoDeuda];
 
-        if (
-          parametros.plazoMaximo < numeroCuotas
-
-        ) {
-          return  [0,0,0,'El número de cuotas supera el plazo maximo establecido para el crédito.'];
+        if (parametros.plazoMaximo < numeroCuotas) {
+          return [0, 0, 0, 'El número de cuotas supera el plazo maximo establecido para el crédito.'];
         }
         if (parametros.montoMaximo < monto) {
-          return [0,0,0,'El monto insgresado supera el monto maximo establecido para el crédito.'];
+          return [0, 0, 0, 'El monto insgresado supera el monto maximo establecido para el crédito.'];
         }
         tasaMensual = tasaInteresComercio[intervalocuota].tasaMensual;
         tasaEA = tasaInteresComercio[intervalocuota].tasaEA;
@@ -261,14 +235,11 @@ export const CALCULOS_UTILS = {
       case 'EDUCATIVO':
         parametros = parametrosEducativo['NINGUNA'];
 
-        if (
-          parametros.plazoMaximo < numeroCuotas
-
-        ) {
-          return  [0,0,0,'El número de cuotas supera el plazo maximo establecido para el crédito.'];
+        if (parametros.plazoMaximo < numeroCuotas) {
+          return [0, 0, 0, 'El número de cuotas supera el plazo maximo establecido para el crédito.'];
         }
         if (parametros.montoMaximo < monto) {
-          return [0,0,0,'El monto insgresado supera el monto maximo establecido para el crédito.'];
+          return [0, 0, 0, 'El monto insgresado supera el monto maximo establecido para el crédito.'];
         }
         tasaMensual = tasaInteresEducativo[intervalocuota].tasaMensual;
         tasaEA = tasaInteresEducativo[intervalocuota].tasaEA;
@@ -276,14 +247,11 @@ export const CALCULOS_UTILS = {
       case 'CREDIFACIL':
         parametros = parametrosCrediFacil['NINGUNA'];
 
-        if (
-          parametros.plazoMaximo < numeroCuotas
-
-        ) {
-          return  [0,0,0,'El número de cuotas supera el plazo maximo establecido para el crédito.'];
+        if (parametros.plazoMaximo < numeroCuotas) {
+          return [0, 0, 0, 'El número de cuotas supera el plazo maximo establecido para el crédito.'];
         }
         if (parametros.montoMaximo < monto) {
-          return [0,0,0,'El monto insgresado supera el monto maximo establecido para el crédito.'];
+          return [0, 0, 0, 'El monto insgresado supera el monto maximo establecido para el crédito.'];
         }
         tasaMensual = tasaInteresCrediFacil[intervalocuota].tasaMensual;
         tasaEA = tasaInteresCrediFacil[intervalocuota].tasaEA;
@@ -296,14 +264,14 @@ export const CALCULOS_UTILS = {
     );
     const division = 1 - tasaPlazo;
     const valorCuota = (interes * monto) / division;
-    return [valorCuota,tasaMensual,tasaEA,''];
+    return [valorCuota, tasaMensual, tasaEA, ''];
   },
   //TODO: Se debe retornar tambien la informacion del error ocurrido y mostrado en un Toast
   calcularInteresAhorro(
     tipoAhorro: TipoAhorro,
     monto: number,
     plazo: number
-  ): [valor:number,interes:number] | null {
+  ): [valor: number, interes: number] | null {
     let parametros;
     let tasaEA = 0;
     let interestotal = 0;
@@ -312,30 +280,21 @@ export const CALCULOS_UTILS = {
       case 'CDAT':
         console.log('se consulta parametros de cdat');
         parametros = parametrosAhorro['NINGUNA'];
-        // console.log('Validacion del monto'+' '+monto+' '+parametros.montoMinimo);
         if (parametros.montoMinimo > monto) {
           return null;
         }
-        // console.log(plazo);
         tasaEA = tasaInteresAhorro[plazo].tasaEA;
-        // console.log(tasaEA);
         var TeaT = 1 + (tasaEA / 100);
-
         var plazoT = (plazo / 365);
-
         var Tea = Math.pow(TeaT, plazoT) - 1;
-
         interestotal = monto * Tea;
         console.log('interestotal cdat ' + interestotal);
         return [interestotal, tasaEA];
-        break;
       case 'CONTRACTUALES':
         parametros = parametrosAhorroContractuales['NINGUNA'];
-        // console.log('Validacion del monto'+' '+monto+' '+parametros.montoMinimo);
         if (parametros.montoMinimo > monto) {
           return null;
         }
-        // console.log(plazo);
         tasaEA = tasaInteresAhorroContractuales[plazo].tasaEA;
         plazo = plazo * 30;
         var TeaT = 1 + (tasaEA / 100);
@@ -345,10 +304,7 @@ export const CALCULOS_UTILS = {
         interestotal = (monto * (valorsup / Tna)) - (monto * plazo / 30);
         console.log('interestotal ' + interestotal);
         return [interestotal, tasaEA];
-        break;
     }
-    //todo al momento de guardar se debe verificar que los parentesis persistan ya que si el autoformateo los quita puede generar error en los calculos
-
+    return null;
   },
 };
-//
